2a99a0b0e1a2ef5a38d5be62c5b43491e2f73210
Leonti Bielski <lbielski@testobject.com>
2016-03-09 09:34:39 +1100

    Add integration test for authentication

--- src/it/scala/E2eSpec.scala
+++ src/it/scala/E2eSpec.scala
@@ -18,31 +18,51 @@ import scala.concurrent.Future
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import scala.concurrent.ExecutionContext.Implicits.global
 
+case class AccessToken(token_type: String, access_token: String, expires_in: Long)
+
 class E2eSpec extends FlatSpec with Matchers with ScalaFutures  with JsonProtocols {
 
+  implicit val accessTokenFormat = jsonFormat3(AccessToken)
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
 
   implicit val defaultPatience =
     PatienceConfig(timeout = Span(15, Seconds), interval = Span(500, Millis))
 
+  def createUser(createUserRequest: CreateUserRequest): Future[UserInfo] = {
+    for {
+      request <- Marshal(createUserRequest).to[RequestEntity]
+      response <- Http().singleRequest(HttpRequest(method = HttpMethods.POST, uri = s"http://localhost:9000/user/create", entity = request))
+      userInfo <- Unmarshal(response.entity).to[UserInfo]
+    } yield userInfo
+  }
+
   it should "create a user" in {
 
     val username = "ci_user_" + java.util.UUID.randomUUID()
     val createUserRequest = CreateUserRequest(username, "password")
 
-    val userInfoFuture: Future[UserInfo] = for {
-      request <- Marshal(createUserRequest).to[RequestEntity]
-      response <- Http().singleRequest(HttpRequest(method = HttpMethods.POST, uri = s"http://localhost:9000/user/create", entity = request))
-      userInfo <- Unmarshal(response.entity).to[UserInfo]
-    } yield userInfo
-
-    whenReady(userInfoFuture) { userInfo =>
+    whenReady(createUser(createUserRequest)) { userInfo =>
 
       userInfo.userName shouldBe username
     }
   }
 
-  // val auth = Authorization(BasicHttpCredentials("joe", "josepp"))
+  it should "authenticate a user" in {
+    val username = "ci_user_" + java.util.UUID.randomUUID()
+    val createUserRequest = CreateUserRequest(username, "password")
 
+    val accessTokenFuture = for {
+      userInfo <- createUser(createUserRequest)
+      response <- Http().singleRequest(HttpRequest(uri = s"http://localhost:9000/token/create",
+        headers = List(Authorization(BasicHttpCredentials(username, "password")))))
+      accessToken <- Unmarshal(response.entity).to[AccessToken]
+    } yield accessToken
+
+    whenReady(accessTokenFuture) { accessToken =>
+      println(accessToken)
+      accessToken.expires_in shouldBe 3600000
+    }
+
+  }
 }
