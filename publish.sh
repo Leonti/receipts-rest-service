914a7ff8fb152ced00ea2134bbb4439397babd4d
Leonti Bielski <lbielski@testobject.com>
2016-03-13 13:02:23 +1100

    Tag succesfull releases from Travis CI

--- publish.sh
+++ publish.sh
@@ -8,6 +8,7 @@ docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t leonti/receipts-rest-service:$version target/scala-2.11/
 docker push leonti/receipts-rest-service
 git tag -a v$version -m 'new version $version'
-#git push --follow-tags
+
+git push --quiet "https://${TAG_TOKEN}@github.com/Leonti/receipts-rest-service" --follow-tags > /dev/null 2>&1
 
 echo "Released version $version"
