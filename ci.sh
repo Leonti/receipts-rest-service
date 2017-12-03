8e7092740019f9aeb4a4595838267678abb086d2
Leonti <prishelec@gmail.com>
2017-12-03 21:07:55 +1100

    Tag github on succesfull release

--- ci.sh
+++ ci.sh
@@ -17,8 +17,8 @@ docker-compose build app
 docker-compose run integration-tests
 docker-compose push app
 
-#git tag -a v$version -m 'new version $version'
+git tag -a v$version -m 'new version $version'
 
-#git push --quiet "https://${TAG_TOKEN}@github.com/Leonti/receipts-rest-service" HEAD:master --follow-tags > /dev/null 2>&1
+git push --quiet "https://${TAG_TOKEN}@github.com/Leonti/receipts-rest-service" HEAD:master --follow-tags > /dev/null 2>&1
 
 echo "Released version $version"
