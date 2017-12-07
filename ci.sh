3dca998f59d24294b57c3c9c086ea5e4499517c5
Leonti <prishelec@gmail.com>
2017-12-07 23:21:14 +1100

    Show app output

--- ci.sh
+++ ci.sh
@@ -4,7 +4,7 @@ set -e
 version=$(date +"%y.%m.%d.%H.%M")
 export VERSION=$version
 
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+#docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
 mkdir -p /tmp/receipts-target
 mkdir -p /tmp/receipts-target-tests
@@ -13,14 +13,17 @@ export AUTH_TOKEN_SECRET="anything"
 export USE_OCR_STUB=true
 
 docker-compose down
-docker-compose run test
+#docker-compose run test
 docker-compose run assembly
 docker-compose build app
-docker-compose run integration-tests
-docker-compose push app
+docker-compose run integration-tests || true
 
-git tag -a v$version -m 'new version $version'
+docker-compose logs app
 
-git push --quiet "https://${TAG_TOKEN}@github.com/Leonti/receipts-rest-service" HEAD:master --follow-tags > /dev/null 2>&1
+#docker-compose push app
+
+#git tag -a v$version -m 'new version $version'
+
+#git push --quiet "https://${TAG_TOKEN}@github.com/Leonti/receipts-rest-service" HEAD:master --follow-tags > /dev/null 2>&1
 
 echo "Released version $version"
