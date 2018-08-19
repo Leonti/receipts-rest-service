3b4aeaa748d912a3418e19d08af031e7830c2ef2
Leonti <prishelec@gmail.com>
2018-08-19 14:00:44 +1000

    Test receipt upload

--- app/src/main/scala/routing/ReceiptEndpoints.scala
+++ app/src/main/scala/routing/ReceiptEndpoints.scala
@@ -4,10 +4,11 @@ import cats.Monad
 import io.finch._
 import io.finch.syntax._
 import model.{ReceiptEntity, User, UserId}
-import service.{FileUploadPrograms, ReceiptPrograms}
+import service.{FileUploadPrograms, ReceiptErrors, ReceiptPrograms}
 import cats.implicits._
 import com.twitter.finagle.http.Status
 import com.twitter.finagle.http.exp.Multipart.FileUpload
+import service.ReceiptErrors.Error
 
 import scala.language.higherKinds
 
@@ -19,6 +20,17 @@ class ReceiptEndpoints[F[_]: ToTwitterFuture: Monad](
 
   private def optionResult[A](o: Option[A]): Output[A] =
     o.fold[Output[A]](Output.failure(new Exception("Entity not found"), Status.NotFound))(v => Ok(v))
+
+  private def eitherResult(e: Either[Error, ReceiptEntity]): Output[ReceiptEntity] = e match {
+    case Right(receipt) => Ok(receipt)
+    case Left(error) =>
+      error match {
+        case ReceiptErrors.FileAlreadyExists() =>
+          Output.failure(new Exception("Can't create receipt with the same file"), Status.BadRequest)
+        case ReceiptErrors.ReceiptNotFound(receiptId) =>
+          Output.failure(new Exception(s"Receipt $receiptId doesn't exist"), Status.NotFound)
+      }
+  }
 
   val getReceipt: Endpoint[ReceiptEntity] =
     get(auth :: "receipt" :: path[String]) { (user: User, receiptId: String) =>
@@ -34,7 +46,7 @@ class ReceiptEndpoints[F[_]: ToTwitterFuture: Monad](
         multipartAttribute("tags")) {
       (user: User, fileUpload: FileUpload, total: String, description: String, transactionTime: String, tags: String) =>
         fileUploadPrograms.toReceiptUpload(fileUpload, total, description, transactionTime, tags).flatMap { receiptUpload =>
-          receiptPrograms.findById(UserId(user.id), "").map(optionResult)
+          receiptPrograms.createReceipt(UserId(user.id), receiptUpload).map(eitherResult)
         }
     }
 
--- app/src/test/scala/ReceiptEndpointsSpec.scala
+++ app/src/test/scala/ReceiptEndpointsSpec.scala
@@ -1,6 +1,8 @@
 import TestInterpreters._
 import authentication.BearerAuth
 import cats.Id
+import com.twitter.finagle.http.{FileElement, RequestBuilder}
+import com.twitter.io.Buf.ByteArray.Owned
 import io.finch.{Endpoint, Input}
 import model.{ReceiptEntity, SubClaim, User}
 import org.scalatest.{FlatSpec, Matchers}
@@ -15,13 +17,51 @@ class ReceiptEndpointsSpec extends FlatSpec with Matchers {
   val randomInt = new RandomInterpreterId("", 0)
   val ocrInt = new OcrInterpreterId()
 
+  private val USER_ID = "123-user"
   val successfulAuth: Endpoint[User] = new BearerAuth[Id, User](
     new TestVerificationAlg(Right(SubClaim(""))),
-    _ => Some(User("id", "email", List()))
+    _ => Some(User(USER_ID, "email", List()))
   ).auth
 
+  it should "create receipt from file upload" in {
+    val receiptRouting = new ReceiptEndpoints[Id](
+      successfulAuth,
+      new ReceiptPrograms(new ReceiptInterpreterId(List(), List()), fileInt, randomInt, ocrInt),
+      new FileUploadPrograms("", fileInt, randomInt)
+    )
+
+    val request = RequestBuilder().url("http://for.builder")
+      .add(FileElement(
+        name = "receipt",
+        content = Owned("file content".getBytes),
+        contentType = Some("application/octet-stream"),
+        filename = Some("receipt.png")
+      ))
+      .addFormElement(
+        "total" -> "12.38",
+        "description" -> "some description",
+        "transactionTime" -> "1480130712396",
+        "tags" -> "veggies,food"
+      ).addHeader("Authorization", "Bearer token")
+      .buildFormPost(multipart = true)
+      .uri("/receipt")
+    val input = Input.fromRequest(request)
+
+    receiptRouting.createReceipt(input).awaitValueUnsafe() shouldBe Some(ReceiptEntity(
+      id = "",
+      userId = USER_ID,
+      total = Some(BigDecimal(12.38)),
+      description = "some description",
+      timestamp = 0,
+      lastModified = 0,
+      transactionTime = 1480130712396l,
+      tags = List("veggies", "food"),
+      files = List()
+    ))
+  }
+
   it should "read receipt by id" in {
-    val receipt = ReceiptEntity(id = "2", userId = "123-user", files = List(), description = "some description")
+    val receipt = ReceiptEntity(id = "2", userId = USER_ID, files = List(), description = "some description")
     val receiptRouting = new ReceiptEndpoints[Id](
       successfulAuth,
       new ReceiptPrograms(new ReceiptInterpreterId(List(receipt), List()), fileInt, randomInt, ocrInt),
