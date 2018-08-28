260b86a5f05d0a57a6e926d79a16efd3c6b4eb3e
Leonti <prishelec@gmail.com>
2018-08-28 20:44:12 +1000

    More cleanup

--- app/src/main/scala/instances/identity.scala
+++ app/src/main/scala/instances/identity.scala
@@ -1,18 +1,11 @@
 package instances
 
 import com.twitter.util.Future
-
-import scala.concurrent.{Future => ScalaFuture}
 import io.finch.syntax.ToTwitterFuture
 import cats.Id
-import routing.ToScalaFuture
 
 object identity {
   implicit val idToTwitterFuture: ToTwitterFuture[Id] = new ToTwitterFuture[Id] {
     def apply[A](f: Id[A]): Future[A] = Future(f)
-  }
-
-  implicit val idToScalaFuture: ToScalaFuture[Id] = new ToScalaFuture[Id] {
-    override def apply[A](f: Id[A]): ScalaFuture[A] = ScalaFuture.successful(f)
   }
 }
